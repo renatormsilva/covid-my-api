ab5b1b8360fa5d4d69acc886883f508f73c32927
renatormsilva <renato.brtkm@gmail.com>
2021-02-09 09:57:50 -0400

    fix: database bug fix

--- src/database/index.js
+++ src/database/index.js
@@ -4,9 +4,12 @@ const Sequelize = require('sequelize');
 const State = require('../models/State')
 
 
-const connection = new Sequelize(process.env.DATABASE_URL, { "dialect": "postgres",
-"dialectOptions": {
-  "ssl": true
+const connection = new Sequelize(process.env.DATABASE_URL,
+  
+  {
+  dialect: "postgres",
+  dialectOptions: {
+  rejectUnauthorized: false
 }});
 
 State.init(connection);
